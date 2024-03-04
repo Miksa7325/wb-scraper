d684f9746a6774e687d4281ff9b7c86c0f5b3c97
Mihail Volodkov <volodkov092@mail.ru>
2024-03-04 21:42:21 +0300

    update product_rating

--- products_data/spider_category_pages.py
+++ products_data/spider_category_pages.py
@@ -15,8 +15,8 @@ class CategorySpider():
         page_source = Selector(text=html)
         for a in page_source.xpath('//*[@id="route-content"]/div/div[2]/div[2]/div[2]/div[2]/div/div/a'):
 
-            product_brand = a.xpath('div[2]/div[3]/span[1]/text()').get()
-            product_name = a.xpath('div[2]/div[3]/span[2]/text()').get()
+            product_brand = a.xpath('div[2]/div[3]/div[2]/span[1]/text()').get()
+            product_name = a.xpath('div[2]/div[3]/div[2]/span[2]/text()').get()
             product_price = a.xpath('div[2]/div[1]/span/span[1]/text()').get()
             if product_price:
                 product_price = float(product_price.replace(',', '.'))
@@ -24,10 +24,15 @@ class CategorySpider():
             product_href = a.xpath('./@href').get()
             product_img = a.xpath('div[1]/div[1]/div/div[1]/div[1]/picture/img/@src').get()
             product_rating = a.xpath('div[2]/div[4]/span[1]/span/text()').get()
-            product_reviews = a.xpath('div[2]/div[4]/span[2]/span/text()').get().replace('\u2009', '')
-            if product_reviews.isdigit():
+            try:
+                product_rating = float(product_rating)
+            except Exception:
+                product_rating = 0
+            product_reviews = a.xpath('div[2]/div[4]/span[2]/span/text()').get()
+            try:
+                product_reviews = product_reviews.replace('\u2009', '')
                 product_reviews = int(product_reviews)
-            else:
+            except Exception:
                 product_reviews = 0
 
             pattern = r"(\S+)(images\S+)"
