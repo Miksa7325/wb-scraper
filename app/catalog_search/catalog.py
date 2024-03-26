e1ac0a780731eb18b7aa116856a7456c6765900e
Mihail Volodkov <volodkov092@mail.ru>
2024-03-26 16:38:14 +0300

    fix catalog.py

--- app/catalog_search/catalog.py
+++ app/catalog_search/catalog.py
@@ -20,16 +20,21 @@ def get_catalog() -> Dict:
     response = requests.get('https://catalog.wb.ru/menu/v10/api', params=params)
 
     for items in response.json().get('data'):
-        if items.get('nodes'):
-            nodes_dict = {}
-            for nodes in items.get('nodes'):
-                if nodes.get('childrenOnly'):
-                    children = {}
-                    for ch in nodes.get('nodes'):
-                        children[ch.get('name')] = f"{url_prefix}{ch.get('id')}"
-                else:
-                    children = f"{url_prefix}{nodes.get('id')}"
+        if not items.get('nodes'):
+            continue
+
+        nodes_dict = {}
+        for nodes in items.get('nodes'):
+            if not nodes.get('childrenOnly'):
+                children = f"{url_prefix}{nodes.get('id')}"
                 nodes_dict[nodes.get('name')] = children
-            categories_dict[items.get('name')] = nodes_dict
+                continue
+
+            children = {}
+            for ch in nodes.get('nodes'):
+                children[ch.get('name')] = f"{url_prefix}{ch.get('id')}"
+            nodes_dict[nodes.get('name')] = children
+
+        categories_dict[items.get('name')] = nodes_dict
 
     return categories_dict
