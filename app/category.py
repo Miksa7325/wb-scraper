7a8229ac844970408f26f3588324463a6b1bdb85
Mihail Volodkov <volodkov092@mail.ru>
2024-03-26 15:46:15 +0300

    fix category.py

--- app/category.py
+++ app/category.py
@@ -1,6 +1,25 @@
 import io
 import json
 
+
+def check_enclosure(el, urls_list, names_list, category_counter):
+    for key, elem in el.items():
+        if not isinstance(elem, dict):
+            print(f'{category_counter}: {key} {elem}')
+            names_list.append(key)
+            urls_list.append(elem)
+            category_counter += 1
+            continue
+
+        for keys, element in elem.items():
+            print(f'{category_counter}: {keys} {element}')
+            names_list.append(keys)
+            urls_list.append(element)
+            category_counter += 1
+
+    return category_counter
+
+
 def get_categories() -> dict[str,str]:
     '''
     return:
@@ -9,24 +28,13 @@ def get_categories() -> dict[str,str]:
     '''
     with io.open('./catalog_search/catalog.json', 'r', encoding='utf-8') as json_file:
         data = json.load(json_file)
+        result_dict = {}
         urls_list = []
         names_list = []
-        result_dict = {}
         category_counter = 0
 
         for k, el in data.items():
-            for key, elem in el.items():
-                if isinstance(elem, dict):
-                    for keys, element in elem.items():
-                        print(f'{category_counter}: {keys} {element}')
-                        names_list.append(keys)
-                        urls_list.append(element)
-                        category_counter += 1
-                else:
-                    print(f'{category_counter}: {key} {elem}')
-                    names_list.append(key)
-                    urls_list.append(elem)
-                    category_counter += 1
+            category_counter = check_enclosure(el, urls_list, names_list, category_counter)
 
         category = 123
         result_dict[names_list[category]] = urls_list[category]
